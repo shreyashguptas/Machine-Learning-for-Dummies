a871da4d070aaf2329e4adb1caac20b8fcf03e9f
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2024-11-22 18:35:11 -0600

    Another v0 fix

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Upgrade pip and install requirements
 python -m pip install --upgrade pip
 pip install -r requirements-book.txt
+
+# Build the Jupyter Book
 jupyter-book build .
-# Add this line to check the contents of the build directory
+
+# List contents of _build/html to verify output
+echo "Contents of _build/html:"
 ls -R _build/html
+
+# Create a simple index.html in the output directory
+echo "<meta http-equiv=\"refresh\" content=\"0; url=./_build/html/index.html\">" > index.html
